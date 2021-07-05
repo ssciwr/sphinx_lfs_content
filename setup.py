a86fafde3d368b7eacf08100a9e28a623477899a
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-07-05 20:33:33 +0200

    Add README as PyPI description

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 from setuptools import find_packages, setup
+import os
+
+
+# Read the contents of README.md
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
 
 setup(
     name="sphinx_lfs_content",
@@ -6,7 +14,8 @@ setup(
     author="Dominic Kempf",
     author_email="dominic.kempf@iwr.uni-heidelberg.de",
     description="Ensure existence of LFS content in your LFS builds",
-    long_description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[
         "requests",

1aed7de728e9f4812570a2b1125d859d9d60fc77
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-07-05 20:48:51 +0200

    Fix classifier

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Operating System :: Linux ",
+        "Operating System :: Unix ",
         "License :: OSI Approved :: MIT License",
     ],
 )

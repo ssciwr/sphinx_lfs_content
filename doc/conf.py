0101c07400fce101a717a9e5121e3a7684de7a37
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2022-03-16 16:11:43 +0100

    Ensure that hooks run even when git-lfs was present on the system

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, Scientific Software Center, Heidelberg University"
 author = "Dominic Kempf"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.0"
+release = "1.1.0"
 
 # -- General configuration ---------------------------------------------------
 

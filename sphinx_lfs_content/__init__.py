4906f9eb7d8409d1bd0cddaf479a11ca2c0449b5
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-11 16:34:35 +0000

    Adding tarfile member sanitization to extractall()

--- sphinx_lfs_content/__init__.py
+++ sphinx_lfs_content/__init__.py
@@ -29,7 +29,26 @@ def lfs_setup(_, config):
 
             # Unpack the tarball
             with tarfile.open(os.path.join(tmp_dir, "git-lfs.tar.gz"), "r:gz") as tar:
-                tar.extractall(path=tmp_dir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path=tmp_dir)
 
             # Setup a modified environment that has the temporary directory in PATH
             # This works around a bug in git-lfs where git-lfs is called recursively,

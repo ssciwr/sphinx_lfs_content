77a43fc2d86bc43a208ad4e3ea7778e2ef8e76ea
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2025-06-26 14:17:26 +0200

    Fixup

--- doc/conf.py
+++ doc/conf.py
@@ -21,7 +21,7 @@ copyright = "2021-2023, Scientific Software Center, Heidelberg University"
 author = "Dominic Kempf"
 
 # The full version, including alpha/beta/rc tags
-release = "1.1.7"
+release = "1.1.9"
 
 # -- General configuration ---------------------------------------------------
 
